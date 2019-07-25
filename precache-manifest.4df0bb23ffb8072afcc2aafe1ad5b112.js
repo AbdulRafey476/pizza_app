a5567aefdf8c2b71bb4d6b892de773aa9a7ba591
Abdul Rafey <abdul.rafey476@hotmail.com>
2019-07-25 13:11:04 -0700

    hi

---
+++ precache-manifest.4df0bb23ffb8072afcc2aafe1ad5b112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb372bc9df564f2bf877b2f0dbdd99a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a8b221ecd29b8659d21",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fb65ae01919e6a8730b9",
+    "url": "/static/css/main.1316e7c8.chunk.css"
+  },
+  {
+    "revision": "0a8b221ecd29b8659d21",
+    "url": "/static/js/2.c89c3ffd.chunk.js"
+  },
+  {
+    "revision": "fb65ae01919e6a8730b9",
+    "url": "/static/js/main.a8363693.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
